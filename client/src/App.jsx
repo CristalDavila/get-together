9ff5500efe46993f04d4d50469c70bcb5d2bdaae
CristalDavila <53529957+CristalDavila@users.noreply.github.com>
2023-10-18 15:47:26 -0600

    Update App.jsx
    updates app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 
+
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import SignUp from "./Components/SignUp";
+import GuestEvent from "./Components/GuestEvent";
+import HostEvent from "./Components/HostEvent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="navbar-title">
+          Get-Together
+          <img
+            src="."
+            className="navbar-image"
+            style={{ width: '150px', height: '200px', borderRadius: '1%' }}
+          />
+        </h1>
+
+        <nav className="navbar navbar-expand-lg custom-navbar">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link">Home</Link></li>
+            <li><Link to={'/Login'} className="nav-link">Login</Link></li>
+            <li><Link to={'/Profile'} className="nav-link">Profile</Link></li>
+            <li><Link to={'/SignUp'} className="nav-link">SignUp</Link></li>
+            <li><Link to={'/GuestEvent'} className="nav-link">GuestEvent</Link></li>
+            <li><Link to={'/HostEvent'} className="nav-link">HostEvent</Link></li>
+          </ul>
+        </nav>
+
+        <hr />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/SignUp' element={<SignUp />} />
+          <Route path='/GuestEvent' element={<GuestEvent />} />
+          <Route path='/HostEvent' element={<HostEvent />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
